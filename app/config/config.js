598c765e03f59e141b60c83501c8ee2ada7307d0
kassim eshemogie <kassim@blusalt.net>
2021-05-19 13:09:38 +0100

    fixed typo

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,5 @@
 let config = {
-    port: process.env.port || 2300,
+    port: process.env.PORT || 2300,
     account: {
         id: '7b124da4-754e-490a-8028-6268a15d5534',
         test_id: '5dc7deff-0f29-420e-8e17-9401fc20c977'
